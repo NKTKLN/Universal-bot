3334366f809571991c137ea100b331f55f5212f6
NKTKLN <nktkln@nktkln.com>
2025-04-09 21:00:22 +0300

    Remove Web App button

--- bot/keyboards/main.py
+++ bot/keyboards/main.py
@@ -8,6 +8,5 @@ def main_menu():
     builder.button(text="🔌 Plugin List")
     builder.button(text="📤 Upload Plugin")
     builder.button(text="⚙️ Settings")
-    builder.button(text="🌐 Web App", web_app=types.WebAppInfo(url="https://yamata-no-orochi.nktkln.com/"))
     builder.adjust(2, 2, 1)
     return builder.as_markup(resize_keyboard=True)
